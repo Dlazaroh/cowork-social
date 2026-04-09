fd97f42e4d178c2d4021d276a7a56eeed78d0d24
Douglas Lázaro <lazarodou@gmail.com>
2026-04-09 13:49:20 -0600

    Acrtualizacion CORS URL despliegue API en la nube

--- backend/Douglas_Lazaro/nodejs/src/middleware/allowCors.js
+++ backend/Douglas_Lazaro/nodejs/src/middleware/allowCors.js
@@ -1,17 +1,16 @@
 const cors = require("cors");
 
 // Middleware para permitir CORS
-const allowDomains = ['http://localhost:5000','https://redes-sociales-8eql.onrender.com','https://redes-sociales-8eql.onrender.com/api-docs/'];
+const allowDomains = ['http://localhost:5000','https://redes-sociales-8eql.onrender.com'];
 const corOptions = {
     origin: function(origin,callback){
         if(allowDomains.indexOf(origin) !== -1 || !origin){
             callback(null,true);
-            optionsSuccessStatus: 200;
-
         }else{
             callback(new Error('No permitido por CORS'));
-        }
-        }
-    }
+            }
+    },
+        optionsSuccessStatus: 200
+};
 
 module.exports = cors(corOptions);
